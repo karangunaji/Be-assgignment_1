7b6f455aaf47889ac019830497d9b7387ec491d6
karan rathod <92873841+karangunaji@users.noreply.github.com>
2023-06-19 09:01:49 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+// home slider
+
+var swiper = new Swiper(".home-slider", {
+    grabCursor:true,
+    loop:true,
+    centeredSlides:true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+ });
+ 
